d3f08e79d92d733b8b57c6937b83357a36e4361c
TomkoSK <tomaslukac599@gmail.com>
2024-03-10 13:18:46 +0100

    port change

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ function saveCanvas(){
     }
   })
 }
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
